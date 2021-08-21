0034b45f85507fa68dff22695aee04a30922e226
Shinichi <s1930557@s.tsukuba.ac.jp>
2021-08-21 10:38:44 +0900

    change dir implement entropy_analysis

--- PCA/PCA_test.py
+++ PCA/PCA_test.py
@@ -5,36 +5,35 @@ import matplotlib.pyplot as plt
 import tkinter
 from tkinter import filedialog
 from tkinter import messagebox
+import math
 from sklearn.decomposition import PCA
 import matplotlib.ticker as ticker
 from sklearn.cluster import KMeans
-
 
 """
 To do 
 指紋領域のみでPCA 
 """
 
-
-extracted_class_num = 15
-x = 401
-y = 101
+extracted_class_num = 5
+x = 41
+y = 41
 
 
 def ask_directory():
     root = tkinter.Tk()
     root.withdraw()
     messagebox.showinfo('select file', 'select analyzing file')
-    image_file_path = tkinter.filedialog.askopenfilename()
-    image_directory = os.path.dirname(image_file_path)
-    os.chdir(image_directory)
+    data_file_path = tkinter.filedialog.askopenfilename()
+    data_directory = os.path.dirname(data_file_path)
+    os.chdir(data_directory)
     messagebox.showinfo('select file', 'select analyzing file')
     axis_path = tkinter.filedialog.askopenfilename()
-    return image_file_path, axis_path
+    return data_file_path, axis_path
 
 
-def PCA_analysis(image_file_path, axis_path):
-    data = pd.read_csv(image_file_path, header=None).T[0:x*y]
+def PCA_analysis(data_file_path, axis_path):
+    data = pd.read_csv(data_file_path, header=None).T[0:x * y]
     x_axis = np.loadtxt(axis_path)
     # data normalization
     norm_data = data.iloc[:, 1:].apply(lambda x: (x - x.mean()) / x.std(), axis=0)
@@ -52,7 +51,7 @@ def PCA_analysis(image_file_path, axis_path):
     plt.xlabel("PC1")
     plt.ylabel("PC2")
     plt.grid(False)
-    plt.savefig("./figures/PCA_scatter_bd.png", transparent=True)
+    plt.savefig("./figures/PCA_scatter.png", transparent=True)
 
     # make graph for CDF
     plt.figure(figsize=(8, 5))
@@ -65,7 +64,7 @@ def PCA_analysis(image_file_path, axis_path):
     plt.grid()
     plt.xlim(0, 15)
     plt.grid(False)
-    plt.savefig("./figures/PCA_CDF_bd.png", transparent=True, bbox_inches="tight", pad_inches=0.1)
+    plt.savefig("./figures/PCA_CDF.png", transparent=True, bbox_inches="tight", pad_inches=0.1)
 
     # clustering and
     extracted_df = data_PCA.iloc[:, 0:extracted_class_num]
@@ -84,7 +83,7 @@ def PCA_analysis(image_file_path, axis_path):
     for i in range(extracted_class_num):
         fig, ax = plt.subplots()
         Class_data = data.T * Class_list[i]
-        Class_data = np.mean(Class_data.T.values, axis = 0)
+        Class_data = np.mean(Class_data.T.values, axis=0)
         ax.set_ylim([-0.2, 1])
         ax.grid(False)
         ax.invert_xaxis()
@@ -92,10 +91,57 @@ def PCA_analysis(image_file_path, axis_path):
         plt.savefig("./spectrum/Class_{}.png".format(i), transparent=True, bbox_inches="tight", pad_inches=0.1)
         pd.DataFrame(Class_data).to_csv("./spectrum/Class_{}.csv".format(i))
 
+def normalization(data_array):
+    amin = np.amin(data_array)
+    amax = np.amax(data_array)
+    scale = 255.0 / (amax - amin)
+    data_array = data_array - amin
+    data_array = data_array * scale
+    data_array = np.uint8(data_array)
+    return data_array
+
+
+
+
+def calc_entropy(tempdata):
+    tempdata = tempdata.values
+    histgram = [0] * 256
+    # normalization
+    tempdata = normalization(tempdata)
+
+    for i in range(len(tempdata)):
+        histgram[tempdata[i]] += 1
+    entropy = 0
+    for i in range(256):
+        p = histgram[i] / len(tempdata)
+        if p == 0:
+            continue
+        entropy -= p * math.log2(p)
+    return entropy
+
+
+def Entropy_analysis(data_file_path, axis_path):
+    data = pd.read_csv(data_file_path, header=None).T  # pixel position * wavenum
+    x_axis = np.loadtxt(axis_path)
+    entropy_values = []
+    for i in range(len(data.T)):
+        tempdata = data[i]
+        entropy_values.append(calc_entropy(tempdata))
+    entropy_df = pd.DataFrame([x_axis, entropy_values])
+    # draw fig
+    fig, ax = plt.subplots()
+    ax.grid(False)
+    ax.invert_xaxis()
+    ax.plot(x_axis, entropy_values, color="black")
+    # save
+    plt.savefig("./figures/entropy_plot.png", transparent=True, bbox_inches="tight", pad_inches=0.1)
+    entropy_df.to_csv("./entropy_df.csv")
+
 
 def main():
-    image_file_path, axis_path = ask_directory()
-    PCA_analysis(image_file_path, axis_path)
+    data_file_path, axis_path = ask_directory()
+    PCA_analysis(data_file_path, axis_path)
+    Entropy_analysis(data_file_path, axis_path)
 
 
 if __name__ == '__main__':
